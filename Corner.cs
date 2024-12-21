df66b944d6ff700af4426d3425c85a8a8f875b71
MagicLiroy <kirill123452919@gmail.com>
2024-12-21 15:16:30 +0300

    Добавьте файлы проекта.

---
+++ Corner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace LabWork10
+{
+    internal class Corner
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else if (value > 180) { Gradus = 180; }
+                else { gradus = value; };
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+            }
+        }
+
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+
+        public Corner(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            double totalGradus = Gradus + (Min / 60.0) + (Sec / 3600.0);
+
+            return totalGradus * (Math.PI / 180);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using LabWork10;
+using System;
+
+namespace HomeWork10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Corner corner;
+
+            do
+            {
+                Console.WriteLine("Введите количество градусов: ");
+                int gradus = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите количество минут");
+                int min = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите количество секунд");
+                int sec = int.Parse(Console.ReadLine());
+
+                corner = new Corner(gradus, min, sec);
+            }
+            while (corner.isCorrect == false);
+
+            double radians = corner.ToRadians();
+
+            Console.WriteLine(radians);
+            Console.ReadKey();
+        }
+    }
+}
